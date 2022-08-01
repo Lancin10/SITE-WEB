634779f1e7ab619848d967750f91571bfd9f8f43
Lancine Sérifou <42044048+Lancin10@users.noreply.github.com>
2022-08-01 15:31:14 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ function Home() {
           <div className="content">
             <h1>Lancine Serifou</h1>
             <h2>Data Engineer | Data Scientist</h2>
-            <h2>Python - R - SQL - PySpark </h2>
+            <h4>Python - R - SQL - PySpark </h4>
             <p>Bonjour et bienvenue !</p>
             <div className="pdf">
               <a href="./media/CV_Lancine_Serifou.PDF" download="CV_Lancine_Serifou.PDF">Télécharger CV</a>
