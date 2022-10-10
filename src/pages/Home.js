ed0943f5decd199cf2bb277eb53c5ba49e61bea4
Lancine Sérifou <42044048+Lancin10@users.noreply.github.com>
2022-10-10 11:12:42 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,12 +22,3 @@ function Home() {
   }
 
 export default Home;
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-Q776FCVE6C"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-Q776FCVE6C');
-</script>
