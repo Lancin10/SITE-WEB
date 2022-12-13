694febafb8c75d66786c94235dbf8dcf47dc0c34
Lancine Sérifou <42044048+Lancin10@users.noreply.github.com>
2022-12-13 15:41:24 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
         <Navigation/>
         <div className="homeContent">
           <div className="content">
-            <h1>Lancine Serifou</h1>
+            <h1>Lancine Sérifou</h1>
             <h2>Data Engineer - Data Scientist - BI</h2>
         
             <p>Bonjour et bienvenue !</p>
