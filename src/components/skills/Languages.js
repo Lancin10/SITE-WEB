f5ab2a49ceca3c7541a66fa44dec464ea6efec03
Lancine Sérifou <42044048+Lancin10@users.noreply.github.com>
2023-08-08 11:21:49 +0200

    Update Languages.js

--- src/components/skills/Languages.js
+++ src/components/skills/Languages.js
@@ -4,16 +4,16 @@ import ProgressBar from './ProgressBar';
 class Languages extends Component {
     state = {
         languages: [
-            {id: 1, value: "R", xp:8.8},
-            {id: 2, value: "Python", xp:7},
-            {id: 3, value: "SQL", xp:7},
+            {id: 1, value: "Python", xp:8.5},
+            {id: 2, value: "R", xp:8},
+            {id: 3, value: "SQL", xp:8},
             {id: 4, value: "Pyspark", xp:6.5},
         ],
         
         Tools: [
-            {id: 1, value: "Shiny", xp:8.5},
-            {id: 3, value: "Power BI", xp:7},
-            {id: 2, value: "Pentaho", xp:7},
+            {id: 1, value: "RShiny", xp:8},
+            {id: 3, value: "Power BI", xp:7.5},
+            {id: 2, value: "Pentaho", xp:7.5},
             {id: 4, value: "Streamlit", xp:6.5},
         ]
     }
